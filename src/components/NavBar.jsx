aa158019d326ef7fa254e2b538f359c69299a4b0
Mario <mario.bravo.82@gmail.com>
2024-07-22 23:51:25 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+const NavBar = () => {
+  return (
+    <div>
+        <h1>SETTING</h1>
+        
+        <CartWidget/>
+    </div>
+  )
+}
+
+export default NavBar

0216785a6c44a39ccf06d0eebd4251e18b280c64
Mario <mario.bravo.82@gmail.com>
2024-07-24 23:54:44 -0300

    Falta Css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+
 import CartWidget from './CartWidget'
+import '../App.css'
+import './NavBar.css'
 
 const NavBar = () => {
   return (
     <div>
         <h1>SETTING</h1>
-        
+        <ul>
+            <li><a href="">Home</a><a href="">Productos</a><a href="">Nosotros</a><a href="">Contacto</a><a href=""></a></li>
+        </ul>
         <CartWidget/>
     </div>
   )
